e9a0891dd32e9e24242be0ad76a6369ae1192132
usbpendrive <ferry.fernando@gmail.com>
2018-12-12 14:57:18 +0700

    Posts module.

---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostsRoutingModule } from './posts-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PostsRoutingModule
+  ]
+})
+export class PostsModule { }
