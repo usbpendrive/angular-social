46658e1a229fc77b3809fe95efae33fcb048a4ee
usbpendrive <ferry.fernando@gmail.com>
2018-12-12 15:02:22 +0700

    Profile module.

---
+++ src/app/posts/containers/posts/posts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  template: `
+    <p>
+      posts works!
+    </p>
+  `,
+  styles: []
+})
+export class PostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
+import { PostsComponent } from './containers/posts/posts.component';
+import { ProfileComponent } from './containers/profile/profile.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PostsComponent, ProfileComponent],
   imports: [
     CommonModule,
     PostsRoutingModule
