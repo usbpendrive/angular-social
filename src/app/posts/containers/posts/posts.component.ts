ec9bd1ef35664ef8f99499b4a5fe0488f718ff1e
usbpendrive <ferry.fernando@gmail.com>
2018-12-12 15:07:15 +0700

    Dummy data test.

--- src/app/posts/containers/posts/posts.component.ts
+++ src/app/posts/containers/posts/posts.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   template: `
-    <p>
-      posts works!
-    </p>
+    <div *ngFor="let post of posts">
+      <a [routerLink]="post.id">
+        {{post.text}}
+      </a>
+    </div>
   `,
   styles: []
 })
 export class PostsComponent implements OnInit {
+  public posts = [];
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 1; i < 10; i++) {
+      this.posts.push({ id: i, text: 'This is post with id: ' + i });
+    }
   }
 
 }
--- src/app/posts/containers/profile/profile.component.ts
+++ src/app/posts/containers/profile/profile.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   template: `
     <p>
-      profile works!
+      {{profile.id}}
     </p>
   `,
   styles: []
 })
 export class ProfileComponent implements OnInit {
+  public profile = { id: null };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(res =>  this.profile.id = 'profileId= ' + res['profileId']);
   }
 
 }
